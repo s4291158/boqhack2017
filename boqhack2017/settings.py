b0cff1903011487ce6398fd47a71737abee1b6a5
Zerong Tony Wang <zerong.wang@uq.net.au>
2017-03-04 16:25:43 +1000

    static & admin settings

--- boqhack2017/settings.py
+++ boqhack2017/settings.py
@@ -106,3 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+if ls.PROD:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
