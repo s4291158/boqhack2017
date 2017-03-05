fc04d0d6af9b204eab39a978c86fa1a6e460af5b
Zerong Tony Wang <zerong.wang@uq.net.au>
2017-03-05 12:11:38 +1000

    pagination and student patch

--- boqhack2017/settings.py
+++ boqhack2017/settings.py
@@ -16,10 +16,16 @@ from corsheaders.defaults import default_headers, default_methods
 
 import local_settings as ls
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ls.SECRET_KEY
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ls.DEBUG
 
 ALLOWED_HOSTS = ['*']
