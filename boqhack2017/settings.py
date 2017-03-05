ea0cdd60d47d7979893fb9c36a2a1cabdd6cdce6
Zerong Tony Wang <zerong.wang@uq.net.au>
2017-03-05 10:50:41 +1000

    cors

--- boqhack2017/settings.py
+++ boqhack2017/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'boqhack2017.urls'
